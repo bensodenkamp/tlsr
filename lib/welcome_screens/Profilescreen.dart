4f8476de6764880c66193e9f7617e604ecfe893f
Ben Sodenkamp <ben@roboticat.com>
2023-04-08 23:10:55 -0600

    brought deps up to date

--- lib/welcome_screens/Profilescreen.dart
+++ lib/welcome_screens/Profilescreen.dart
@@ -325,18 +325,18 @@ checked=!checked;
             child: Padding(
               padding: EdgeInsets.only(right: 10.0),
               child: Container(
-                  child: new RaisedButton(
+                  child: new ElevatedButton(
                     child: new Text("Save"),
-                    textColor: Color.fromRGBO(242, 169, 184, 1),
-                    color: Colors.green,
+                    //textColor: Color.fromRGBO(242, 169, 184, 1),
+                    //color: Colors.green,
                     onPressed: () {
                       setState(() {
                         _status = true;
                         FocusScope.of(context).requestFocus(new FocusNode());
                       });
                     },
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(20.0)),
+                    //shape: new RoundedRectangleBorder(
+                     //   borderRadius: new BorderRadius.circular(20.0)),
                   )),
             ),
             flex: 2,
@@ -345,18 +345,18 @@ checked=!checked;
             child: Padding(
               padding: EdgeInsets.only(left: 10.0),
               child: Container(
-                  child: new RaisedButton(
+                  child: new ElevatedButton(
                     child: new Text("Cancel"),
-                    textColor: Colors.white,
-                    color: Colors.red,
+                    //textColor: Colors.white,
+                    //color: Colors.red,
                     onPressed: () {
                       setState(() {
                         _status = true;
                         FocusScope.of(context).requestFocus(new FocusNode());
                       });
                     },
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(20.0)),
+                    //shape: new RoundedRectangleBorder(
+                    //    borderRadius: new BorderRadius.circular(20.0)),
                   )),
             ),
             flex: 2,
